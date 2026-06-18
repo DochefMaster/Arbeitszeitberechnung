bfd70cc4f89e6f8d6dcb3544174c313538215bef
Dominik Reindl <dominik.reindl@ksmenzingen.ch>
2026-06-18 13:39:19 +0200

    Zeitberechnung flexibler gestaltet, Anzahl Ferien, mitgenommene Überzeit und wie viele Brückentage gearbeitet werden wurden variabel in die Funktion eingefügt

--- berechnung.py
+++ berechnung.py
@@ -1,24 +1,38 @@
 # Daten für das Jahr 2026
 ARBEITSTAGE_JAHR_5T = 250  # Mit 100% Anstellung und 5 Wochen Ferien
 ARBEITSSTUNDEN_JAHR_VOLLZEIT = 2100  # Mit 100% Anstellung und 5 Wochen Ferien
-SCHULFERIEN_WOCHEN = 14
-ARBEITSFERIEN_WOCHEN = 5
+SCHULFERIEN_WOCHEN = 14  # Aus Jahresplan
 
 
-def berechnung_arbeitsstunden_tag(anzahl_arbeitstage_pro_woche, arbeits_pensum, arbeitstage_ferien):
+def umrechnung_zeit(zeit_dezimal):
+    return int(zeit_dezimal), int(round((zeit_dezimal - int(zeit_dezimal)) * 60, 0))
+
+
+
+"""
+anzahl_arbeitstage_pro_woche: Wird vom User eingegeben
+arbeits_pensum: Wird vom User eingegeben
+arbeitstage_ferien: Wird aus mehreren Eingaben im GUI berechnet
+vorhandene_ueberzeit: Wird vom User eingegeben
+brueckentage_frei: Wird vom User eingegeben
+"""
+
+
+def berechnung_arbeitsstunden_tag(anzahl_arbeitstage_pro_woche, arbeits_pensum, arbeitstage_ferien, arbeitsferien_woche, vorhandene_ueberzeit=0, brueckentage_frei=0,):
     # Jahressoll auf auf Pensum skalieren
-    arbeitsstunden_jahr_teilzeit = ARBEITSSTUNDEN_JAHR_VOLLZEIT * arbeits_pensum / 100
-    # Das komplette Jahr (Arbeitstage) auf Pensum skalieren
+    arbeitsstunden_jahr_teilzeit = ARBEITSSTUNDEN_JAHR_VOLLZEIT * arbeits_pensum / 100 - vorhandene_ueberzeit
+    # Das komplette Jahr (Arbeitstage) auf x Tagewoche skalieren
     arbeitstage_jahr_teilzeit = ARBEITSTAGE_JAHR_5T * (anzahl_arbeitstage_pro_woche / 5)
     # Zwischenrechnung wie viele Tage man zusätzlich noch weg ist, über die bezahlten ferien hinaus
-    schulferien_unbezahlt_tage_teilzeit = (SCHULFERIEN_WOCHEN - ARBEITSFERIEN_WOCHEN) * anzahl_arbeitstage_pro_woche
-    abwesenheit_schulferien_tage = schulferien_unbezahlt_tage_teilzeit - arbeitstage_ferien
+    schulferien_unbezahlt_tage_teilzeit = (SCHULFERIEN_WOCHEN - arbeitsferien_woche) * anzahl_arbeitstage_pro_woche
+    abwesenheit_schulferien_tage = schulferien_unbezahlt_tage_teilzeit - arbeitstage_ferien + brueckentage_frei
     # Tagessoll berechnen mit allen Abhängigkeiten
-    return (arbeitsstunden_jahr_teilzeit / (arbeitstage_jahr_teilzeit - abwesenheit_schulferien_tage))
+    tagessoll_dezimal = (arbeitsstunden_jahr_teilzeit / (arbeitstage_jahr_teilzeit - abwesenheit_schulferien_tage))
+    return umrechnung_zeit(tagessoll_dezimal)
 
 
 # --- Test-Bereich für das Terminal ---
 if __name__ == "__main__":
 
-    berechneter_wert = berechnung_arbeitsstunden_tag(4, 70, 13)
-    print("Der Testdurchlauf ergab: ", berechneter_wert)
+    stunden, minuten = berechnung_arbeitsstunden_tag(4, 70, 15, 5, 0, 0)
+    print("Der Testdurchlauf ergab: ", stunden, "h ", minuten, "min")
